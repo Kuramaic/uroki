06851c3fb07d8871a0797e2ce371978b7a4b9809
KURAMA <darfagel@gmail.com>
2023-12-23 22:09:53 +0500

    321

---
+++ задачи 1/13-15.py
@@ -0,0 +1,29 @@
+#13
+# def to_dickt(lst):
+#     dicktum=dict()
+#     for i in lst:
+#         dicktum[i]=i
+#     print(dicktum)
+
+# m= [1,23,4,7,5453,0]
+# to_dickt(m)
+
+
+#14
+# my_dict = {'first_one': 'we can do it'}
+# def biggest_dict(**kwargs):
+#     my_dict.update(**kwargs)
+# biggest_dict(k1=22, k2=31, k3=11, k4=91)
+# biggest_dict(name='Елена', age=31, weight=61, eyes_color='grey')
+# print(my_dict)
+
+#15
+# def count_it(a):	
+#     m=[]
+#     dick={}
+#     for i in '0123456789':
+#         sum=a.count(i)
+#         m.append(sum)
+    
+
+#16
---
+++ задачи 2/1-15.py
@@ -0,0 +1,30 @@
+#1
+# x=[1,43,2,5,67,3]
+# y=[4,5,3,4,2,56]
+# c=[]
+# for i in range(len(x)):
+#     m=[]
+#     m.append(x[i])
+#     m.append(y[i])
+#     c.append(m)
+# print(c)
+
+#2
+# a=['Аутист', 'Наркоман', 'Алкоголик', 'ДЦП', 'Клоун']
+# def privet(a):
+#     for i in a:
+#         print('Привет! '+i)
+# privet(a)
+
+
+#3
+# def dooble(a):
+#     m=[]
+#     for i in range(len(a)):
+#         m.append(a[i])
+#     print(m)
+#     for i in range(len(m)-1):
+#         if m[i]==m[i+1]:
+#             return True
+#     return False
+# print(dooble('popajopaa'))
