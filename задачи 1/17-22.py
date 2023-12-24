3a4d0d6bbe672d29cbe3a6f3e5f36922d1607e38
KURAMA <darfagel@gmail.com>
2023-12-24 20:18:00 +0500

    rwe

--- задачи 1/17-22.py
+++ задачи 1/17-22.py
@@ -14,11 +14,46 @@
 #     return (None,None)
 # print(first_last('j','kjkjkjlkjljdkljljklkjlkj'))
 
-#19
+# #19
+# from collections import Counter
+# def top3(st):
+#     counter_top3 = Counter(st.replace(' ', '')).most_common(3)
+#     print(counter_top3)
+#     return ', '.join([f'{letter} - {cnt}' for letter, cnt in counter_top3])
+# top3("fsdjkfksdljfhkjfghgfhnmnvytopotypcxmnvmrpwerwer")
+        
+#20
+# def camel(st):
+#     s=''
+#     count = 0
+#     for i,v in enumerate(st):
+#         if v.isalpha():
+#             if count%2==0:
+#                 s+=v.upper()
+#             else:
+#                 s+=v.lower()
+#             count+=1
+#         else:
+#             s+=v
+#     return s
+# print(camel('fdsssssdsdfsdf fgsdfsdf  fsdfsdf'))
 
-def top3(st):
-    m=[]
-    alfavit='абвгдеёжзиёклмнопрстуфхцчшщьыъэюя'
-    for i in alfavit:
-        
-        
+#21
+# def shortener(st):
+#     while '(' in st or ')' in st:
+#         l = st.rfind('(')
+#         r = st.find(')', l)
+#         st = st.replace(st[l:r + 1], '')
+#     return st
+# print(shortener('fdsffffffffffdsfdsdfsd(fsdsddfsdf) dsfaad (fsdffsd)'))
+
+#22
+# def cleaned_str(st):
+#     m = []
+#     for i in st:
+#         if i == '@' and m:
+#             m.pop()
+#         elif i != '@':
+#             m.append(i)
+#     return ''.join(m)
+# print(cleaned_str('poo@pa joo@pa'))
