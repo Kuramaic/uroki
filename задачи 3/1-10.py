14a49fc036289743bc108e40b692dd4e9338f396
KURAMA <darfagel@gmail.com>
2023-12-25 21:05:35 +0500

    3e21

---
+++ задачи 3/1-10.py
@@ -0,0 +1,32 @@
+# #1
+# a=input()
+# b=input()
+# if a==b:
+#     print('Draw')
+# else:
+#     if a=='paper':
+#         if b=='rock':
+#             print('1 Won!')
+#         else:
+#             print('2 Won!')
+#     if a=='rock':
+#         if b=='paper':
+#             print('2 Won!')
+#         else:
+#             print('1 Won!')
+#     if a=='scissors':
+#         if b=='paper':
+#             print('1 Won!')
+#         else:
+#             print('2 Won!')
+
+
+#2
+# def money(m):
+#     n=len(m)/3
+#     if n==int(n):
+#         return 'DA'
+#     return 'He'
+# print(money(['popa', 'popa', 'popa', 'popa', 'popa', 'popa'])) 
+
+#3
